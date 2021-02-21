c48b84c716f54eb4e212adb853b74532033caec8
zuev720 <zuev720mail.ru>
2021-02-21 20:18:39 +0300

    add files

--- src/app.js
+++ src/app.js
@@ -5,4 +5,5 @@ const characters = [
   { name: 'лучник', health: 0 },
 ];
 
-characters.filter((item) => item.health > 0);
+const alive = characters.filter((item) => item.health > 0);
+console.log(alive);
